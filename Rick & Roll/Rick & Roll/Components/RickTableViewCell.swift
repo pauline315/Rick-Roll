51660a085f958834be646f2d80cb0dac11809f13
lexa-com <mwathijoel@gmail.com>
2024-09-25 11:37:36 +0300

    Rxswift resolve and Image downloader

--- Rick & Roll/Rick & Roll/Components/RickTableViewCell.swift
+++ Rick & Roll/Rick & Roll/Components/RickTableViewCell.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class RickTableViewCell: UITableViewCell {
+    static let cellId = "characterCell"
     
     var characterName    = UILabel()
     var characterSpecies = UILabel()
@@ -15,8 +16,6 @@ class RickTableViewCell: UITableViewCell {
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super .init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        setCellUi()
     }
     
     required init?(coder: NSCoder) {
@@ -26,6 +25,7 @@ class RickTableViewCell: UITableViewCell {
     func bindCellData(character:Character){
         characterName.text = character.name
         characterSpecies.text = character.species
+        characterImage.loadImage(urlString: character.image)
     }
     
     
@@ -34,36 +34,39 @@ class RickTableViewCell: UITableViewCell {
         addSubview(characterSpecies)
         addSubview(characterImage)
         
-        characterName.font = .systemFont(ofSize: 20, weight: .bold)
+        characterName.font = .systemFont(ofSize: 28, weight: .bold)
         characterName.translatesAutoresizingMaskIntoConstraints = false
         
-        characterSpecies.font = .systemFont(ofSize: 12, weight: .semibold)
+        characterSpecies.font = .systemFont(ofSize: 20, weight: .semibold)
         characterSpecies.translatesAutoresizingMaskIntoConstraints = false
         
         characterImage.layer.cornerRadius = 12
+        characterImage.clipsToBounds      = true
         characterImage.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             //image constraints
-            characterImage.leadingAnchor.constraint(equalTo: leadingAnchor,constant: 20),
-            characterImage.topAnchor.constraint(equalTo: topAnchor, constant: 20),
-            characterImage.heightAnchor.constraint(equalToConstant: 80),
-            characterImage.widthAnchor.constraint(equalToConstant: 80),
+            characterImage.leadingAnchor.constraint(equalTo: leadingAnchor,constant: 30),
+            characterImage.centerYAnchor.constraint(equalTo: centerYAnchor),
+            characterImage.heightAnchor.constraint(equalToConstant: 120),
+            characterImage.widthAnchor.constraint(equalToConstant: 120),
             
             
             characterName.leadingAnchor.constraint(equalTo:characterImage.trailingAnchor, constant: 20),
-            characterName.topAnchor.constraint(equalTo: topAnchor, constant: 20),
-            characterName.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            characterName.topAnchor.constraint(equalTo: characterImage.topAnchor, constant: 8),
+          
             
             //character spec
             
             characterSpecies.leadingAnchor.constraint(equalTo: characterImage.trailingAnchor, constant: 20),
             characterSpecies.topAnchor.constraint(equalTo: characterName.bottomAnchor, constant: 8),
-            characterSpecies.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20)
-            
+                       
         
         ])
         
     }
     
+}
+#Preview{
+    RickTableViewCell()
 }
